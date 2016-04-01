12be9cdeb6b943fd8c8de540698bd2bf18c261ae
AmberJMLee <alphagirl93@yahoo.com>
2016-04-01 17:10:12 -0400

    Imported stuff.

--- StudyHome/app/src/main/java/cs4720/studyhome/AppListActivity.java
+++ StudyHome/app/src/main/java/cs4720/studyhome/AppListActivity.java
@@ -9,6 +9,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
