79ccab65e64782dc082f621db474ef481c3660de
AmberJMLee <alphagirl93@yahoo.com>
2016-04-04 18:45:36 -0400

    Get JSONObject.

--- StudyHome/app/src/main/java/cs4720/studyhome/HomeActivity.java
+++ StudyHome/app/src/main/java/cs4720/studyhome/HomeActivity.java
@@ -8,11 +8,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -27,6 +33,9 @@ public class HomeActivity extends AppCompatActivity {
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        JSONObject jsonobject = getJSON();
+        
         final Intent i = new Intent(this, AppListActivity.class);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +45,36 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
+    public static JSONObject getJSON(){
+        try{
+            URL url;
 
+            url = new URL("http://api.openweathermap.org/data/2.5/weather?q=Sacramento%2CUS&APPID=b9b4115aed280f077d476837f748b4a4");
+            URLConnection connection = (URLConnection)url.openConnection();
+            connection.addRequestProperty("x-api-key", "11111");
+            Log.e("Status", "Connection set");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            Log.e("Status", "Reader set");
+
+            StringBuffer json = new StringBuffer(1024);
+            String tmp="";
+            while((tmp=reader.readLine())!=null){
+                json.append(tmp).append("\n");
+            }
+            reader.close();
+            JSONObject data = new JSONObject(json.toString());
+
+            // This value will be 404 if the request was not successful
+            if(data.getInt("cod") != 200){
+                return null;
+            }
+            return data;
+        }
+        catch(Exception e){
+            return null;
+        }
+    }
     public void showApps(View v){
 
     }
