49987b469f984c39106e27557f1bb41e681fe2d3
Amir <ag5ct@virginia.edu>
2016-04-16 01:04:22 -0400

    Worked up to page 2

--- StudyHome/app/src/main/java/cs4720/studyhome/PageFragment.java
+++ StudyHome/app/src/main/java/cs4720/studyhome/PageFragment.java
@@ -9,6 +9,9 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -21,6 +24,9 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -41,14 +47,23 @@ public class PageFragment extends android.support.v4.app.Fragment {
         View view = inflater.inflate(R.layout.page_layout, container, false);
         //textview = (TextView) view.findViewById(R.id.textview);
         Bundle bundle = getArguments();
-        String page = Integer.toString(bundle.getInt("count"));
+        int page = bundle.getInt("count");
         //textview.setText("This is the message: "+message+ "Swipe view page");
         RelativeLayout relativeLayout = (RelativeLayout) view.findViewById(R.id.layout);
-        if (page.equals("1")) {
+        //relativeLayout.setLeft(500);
+        if (page == 1) {
             final TextView view2 = new TextView(getContext());//(TextView) view.findViewById(R.id.weather);
             view2.setTextColor(Color.parseColor("#00CCFF"));
             view2.setGravity(Gravity.CENTER);
             view2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 40);
+
+            final TextView view3 = new TextView(getContext());
+            view3.setTextColor(Color.parseColor("#00CCFF"));
+            view3.setGravity(Gravity.CENTER);
+            //view3.setGravity(Gravity.H)
+            view3.setY(100);
+            view3.setTextSize(TypedValue.COMPLEX_UNIT_SP, 33);
+
             final RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.FILL_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
             //params.addRule(RelativeLayout.)
             RequestQueue queue = Volley.newRequestQueue(getContext());
@@ -63,6 +78,7 @@ public class PageFragment extends android.support.v4.app.Fragment {
 
                             JSONObject jsonobject;
                             JSONObject jsonweather;
+                            JSONObject jsonlocation;
                             try {
                                 jsonobject = new JSONObject(response);
                                 String weatherData = jsonobject.getString("weather");
@@ -70,6 +86,8 @@ public class PageFragment extends android.support.v4.app.Fragment {
                                 weatherData = weatherData.replace("]", "");
                                 jsonweather = new JSONObject(weatherData);
                                 view2.setText(jsonweather.getString("main"));
+                                //String locationData = jsonobject.getString("sys");
+                                view3.setText(jsonobject.getString("name"));
 
                             } catch (JSONException e) {
                                 view2.setText(response);
@@ -86,9 +104,30 @@ public class PageFragment extends android.support.v4.app.Fragment {
             });
             // Add the request to the RequestQueue.
             queue.add(stringRequest);
+
+            //group.addView(view2);
+
             relativeLayout.addView(view2, params);
+            relativeLayout.addView(view3, params);
+        }
+        if (page == 2) {
+            loadLocations();
+            loadLocationList();
+            addClickListener();
         }
         return view;
     }
-
+    private ArrayList<StudyGroup> studygroups = new ArrayList<StudyGroup>();
+    private void loadLocations() {
+        //Get study groups from a database
+        //Add to studygroups arraylist
+        //Sort by classes that student is taking
+    }
+    private GridView list;
+    private void loadLocationList() {
+        //Populate the screen with the items
+    }
+    private void addClickListener() {
+        //When you click the item, it will take you to a map that gets you to the location
+    }
 }
---
+++ StudyHome/app/src/main/java/cs4720/studyhome/StudyGroup.java
@@ -0,0 +1,12 @@
+package cs4720.studyhome;
+
+import java.sql.Timestamp;
+/**
+ * Created by Amir on 4/16/2016.
+ */
+public class StudyGroup {
+    CharSequence className;
+    CharSequence building;
+    int size;
+    Timestamp timestamp;
+}
