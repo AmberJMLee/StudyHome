ee57ee28a6949d4802bd9731981f4d97d2c2f26f
Amir <ag5ct@virginia.edu>
2016-04-15 23:29:48 -0400

    Only one page shows weather.

---
+++ StudyHome/app/src/main/java/cs4720/studyhome/PageFragment.java
@@ -0,0 +1,83 @@
+package cs4720.studyhome;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PageFragment extends android.support.v4.app.Fragment {
+    TextView textview;
+
+    public PageFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.page_layout, container, false);
+        //textview = (TextView) view.findViewById(R.id.textview);
+        Bundle bundle = getArguments();
+        String page = Integer.toString(bundle.getInt("count"));
+        //textview.setText("This is the message: "+message+ "Swipe view page");
+        if (page.equals("1")) {
+            final TextView view2 = (TextView) view.findViewById(R.id.weather);
+            RequestQueue queue = Volley.newRequestQueue(getContext());
+            String url = "http://api.openweathermap.org/data/2.5/weather?q=Sacramento%2CUS&APPID=b9b4115aed280f077d476837f748b4a4";
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // Display the first 500 characters of the response string.
+                            //view.setText("Response is: "+ response);
+
+
+                            JSONObject jsonobject;
+                            JSONObject jsonweather;
+                            try {
+                                jsonobject = new JSONObject(response);
+                                String weatherData = jsonobject.getString("weather");
+                                weatherData = weatherData.replace("[", "");
+                                weatherData = weatherData.replace("]", "");
+                                jsonweather = new JSONObject(weatherData);
+                                view2.setText(jsonweather.getString("main"));
+
+                            } catch (JSONException e) {
+                                view2.setText(response);
+                                e.printStackTrace();
+                            }
+
+                            //.substring(0,50));
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    view2.setText("That didn't work!");
+                }
+            });
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest);
+        }
+        return view;
+    }
+
+}
---
+++ StudyHome/app/src/main/java/cs4720/studyhome/StudyGroupActivity.java
@@ -0,0 +1,8 @@
+package cs4720.studyhome;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+public class StudyGroupActivity extends FragmentActivity {
+}
