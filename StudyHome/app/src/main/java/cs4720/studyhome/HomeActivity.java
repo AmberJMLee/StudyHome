c4f869659ebde32f4714a0204c7498c032841bd8
Amir <ag5ct@virginia.edu>
2016-04-15 23:24:13 -0400

    Initial commit.

--- StudyHome/app/src/main/java/cs4720/studyhome/HomeActivity.java
+++ StudyHome/app/src/main/java/cs4720/studyhome/HomeActivity.java
@@ -1,12 +1,15 @@
 package cs4720.studyhome;
 
 import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -36,10 +39,11 @@ import java.text.DecimalFormat;
 //import com.android.volley.RequestQueue;
 //import com.android.volley;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends FragmentActivity {
     private double temperature, humidity, pressure;
     String description;
-
+    ActionBar actionbar;
+    ViewPager viewpager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +54,10 @@ public class HomeActivity extends AppCompatActivity {
         ViewGroup weatherwidget = (ViewGroup) getLayoutInflater().inflate(R.layout.weather_widget, null);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        actionbar = getActionBar();
+        viewpager = (ViewPager) findViewById(R.id.view_pager);
+        SwipeAdapter swipeadapter = new SwipeAdapter(getSupportFragmentManager());
+        viewpager.setAdapter(swipeadapter);
 
 //        JSONObject jsonobject = getJSON();
 //        try {
@@ -58,42 +66,6 @@ public class HomeActivity extends AppCompatActivity {
 //            e.printStackTrace();
 //        }
         //retrieveWeather();
-        final TextView view = (TextView) this.findViewById(R.id.weather);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://api.openweathermap.org/data/2.5/weather?q=Sacramento%2CUS&APPID=b9b4115aed280f077d476837f748b4a4";
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        //view.setText("Response is: "+ response);
-
-
-                        JSONObject jsonobject;
-                        JSONObject jsonweather;
-                        try {
-                            jsonobject = new JSONObject(response);
-                            String weatherData = jsonobject.getString("weather");
-                            weatherData = weatherData.replace("[", "");
-                            weatherData = weatherData.replace("]", "");
-                            jsonweather = new JSONObject(weatherData);
-                            view.setText(jsonweather.getString("main"));
-
-                        } catch (JSONException e) {
-                            view.setText(response);
-                            e.printStackTrace();
-                        }
-
-                        //.substring(0,50));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                view.setText("That didn't work!");
-            }
-        });
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
 
 
 
@@ -139,6 +111,21 @@ public class HomeActivity extends AppCompatActivity {
     public void SetHumidity(double humidity) {
         this.humidity = humidity;
     }
+
+//    @Override
+//    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+//
+//    }
+//
+//    @Override
+//    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+//
+//    }
+//
+//    @Override
+//    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+//
+//    }
 }
 
 
