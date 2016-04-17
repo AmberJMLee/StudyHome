7f285b4fd1687855b10d510039677f68ae52df29
Amir <ag5ct@virginia.edu>
2016-04-17 00:46:49 -0400

    Possible saving???

---
+++ StudyHome/app/src/main/java/cs4720/studyhome/DatabaseHelper.java
@@ -0,0 +1,26 @@
+package cs4720.studyhome;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "StudyHome.db";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table my_table (compid varchar(25), name varchar(25))");
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL("delete table my_table");
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
